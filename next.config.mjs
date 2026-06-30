0561f5744e19bc32634cf46f6c65d5e180e7e763
bryanping <45011395+bryanping@users.noreply.github.com>
2026-06-30 23:38:55 +0800

    v5 fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(Array.isArray(config.externals) ? config.externals : []), '@opentelemetry/api']
-    } else {
-      config.resolve.fallback = { ...config.resolve.fallback, '@opentelemetry/api': false }
-    }
-    return config
-  },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   images: {
